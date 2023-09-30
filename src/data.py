a82805005d6f33a0ddaf5e6e5d231d6a1de55a98
Gennadiy <renardes@gmail.com>
2023-09-30 11:03:44 +0500

    функции и глобальные переменные для работы с файлами данных

--- src/data.py
+++ src/data.py
@@ -2,4 +2,36 @@
 Вспомогательный модуль — глобальные переменные и условные константы.
 """
 
+# стандартная библиотека
+from pathlib import Path
+from sys import path, argv
+
+
+# "флаг" внутреннего режима тестирования и отладки
+DEBUG: bool = 'debug' in argv
+# выводимые данные
+debug_data: dict = {}
+
+
+# переменные для аннотаций
+Players = dict[str, dict[str, int]]
+Saves = dict[tuple[str, str], tuple[list[int], int]]
+
+
+# корень проекта
+ROOT_DIR = Path(path[0]).parent
+# каталог данных
+DATA_DIR = ROOT_DIR / 'data'
+
+# модификация путей для тестовых файлов данных
+test_path = 'test/' if DEBUG else ''
+# пути к файлам данных
+players_path = DATA_DIR / f'{test_path}players.ini'
+saves_path = DATA_DIR / f'{test_path}saves.ttt'
+
+
+# база игроков
+players_db: Players = {}
+# база сохранений
+saves_db: Saves = {}
 
--- src/files.py
+++ src/files.py
@@ -2,4 +2,62 @@
 Вспомогательный модуль — чтение/запись файлов данных.
 """
 
+# стандартная библиотека
+from configparser import ConfigParser
+# текущий проект
+import data
+
+
+def read_players() -> data.Players:
+    """"""
+    players = ConfigParser()
+    players.read(data.players_path)
+    return {
+        player: {
+            k: int(v)
+            for k, v in players[player].items()
+        }
+        for player in players.sections()
+    }
+
+
+def read_saves() -> data.Saves:
+    """"""
+    raw = data.saves_path.read_text(encoding='utf-8').strip().split('\n')
+    saves = {}
+    for save in raw:
+        try:
+            players, turns, dim = save.split('!')
+        # пустая строчка в файле
+        except ValueError:
+            continue
+        players = tuple(players.split(','))
+        try:
+            turns = [int(t) for t in turns.split(',')]
+        # сохранение до первого хода
+        except ValueError:
+            turns = []
+        saves[players] = (turns, int(dim))
+    return saves
+
+
+def write_players() -> None:
+    """"""
+    players = ConfigParser()
+    players.read_dict(data.players_db)
+    with open(data.players_path, 'w', encoding='utf-8') as fileout:
+        players.write(fileout)
+
+
+def write_saves() -> None:
+    """"""
+    saves = []
+    for players, save in data.saves_db.items():
+        players = ','.join(players)
+        turns, dim = save
+        turns = ','.join(map(str, turns))
+        saves.append(f"{players}!{turns}!{dim}")
+    saves = '\n'.join(saves)
+    data.saves_path.write_text(saves, encoding='utf-8')
+
 
